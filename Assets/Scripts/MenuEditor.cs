b4f36584d8de0a6867b9b0724b5e4d7dca5913ad
hotte1411 <hotte1411@gmail.com>
2023-01-12 12:47:01 +0400

    some changes

--- Assets/Scripts/MenuEditor.cs
+++ Assets/Scripts/MenuEditor.cs
@@ -10,23 +10,15 @@ public class MenuEditor : MonoBehaviour
     [SerializeField] private Transform itemPrefab;
     [SerializeField] RectTransform contentAclohol;
 
-    private List<Item> items = new();
+    private List<ItemModel> items = new();
 
     private void Start()
     {
-        
-        items.Add(new Item("Vodka", "vodka", 0.5f, 40, 1000));
-        items.Add(new Item("Lager", "beer", 0.5f, 8, 600));
-        items.Add(new Item("AppleCidre", "cidre", 0.3f, 3, 1200));
+        items.Add(new ItemModel("Vodka", "vodka", 0.5f, 40, 1000));
+        items.Add(new ItemModel("Lager", "beer", 0.5f, 8, 600));
+        items.Add(new ItemModel("AppleCidre", "cidre", 0.3f, 3, 1200));
 
-       
-        foreach(Transform child in contentAclohol)
-        {
-           Destroy(child.gameObject);
-        }
-        
-        
-        foreach(Item item in items)
+        foreach (ItemModel item in items)
         {
             var instance = GameObject.Instantiate(itemPrefab.gameObject, contentAclohol);
             instance.transform.GetChild(0).GetChild(0).gameObject.GetComponent<TMP_Text>().text = item.label;
@@ -34,6 +26,25 @@ public class MenuEditor : MonoBehaviour
             instance.transform.GetChild(2).GetChild(0).gameObject.GetComponent<TMP_Text>().text = item.volume.ToString();
             instance.transform.GetChild(3).GetChild(0).gameObject.GetComponent<TMP_Text>().text = item.strength.ToString();
             instance.transform.GetChild(4).GetChild(0).gameObject.GetComponent<TMP_Text>().text = item.price.ToString();
+        }
+    }
+
+
+    class ItemModel
+    {
+        public string label { get; private set; }
+        public string type { get; set; }
+
+        public float volume, strength, price;
+        private int id, order;
+
+        public ItemModel(string label, string type, float volume, float strength, float price)
+        {
+            this.label = label;
+            this.type = type;
+            this.volume = volume;
+            this.strength = strength;
+            this.price = price;
         }
     }
 }
