488df9b65c0d9fdc62bb4aea58ecf7b4517c1ed5
hotte1411 <hotte1411@gmail.com>
2023-01-13 14:27:44 +0400

    autosave

---
+++ Assets/Scripts/AlcoholItem.cs
@@ -0,0 +1,22 @@
+/*using System;
+using UnityEngine;
+
+[Serializable]
+public class AlcoholItem : MonoBehaviour
+{
+    public string label { get; private set; }
+    public string type { get; private set; }
+    public float volume { get; private set; }
+    public float strength { get; private set; }
+    public float price { get; private set; }
+
+    public AlcoholItem(string label, string type, float volume, float stregth, float price)
+    {
+        this.label = label ?? throw new ArgumentNullException(nameof(label));
+        this.type = type ?? throw new ArgumentNullException(nameof(type));
+        this.volume = volume;
+        this.strength = stregth;
+        this.price = price;
+    }
+}
+*/
--- Assets/Scripts/MenuEditor.cs
+++ Assets/Scripts/MenuEditor.cs
@@ -7,43 +7,92 @@ using TMPro;
 
 public class MenuEditor : MonoBehaviour
 {
-    [SerializeField] private Transform itemPrefab;
+    [SerializeField] Transform alcoholItemPrefab;
     [SerializeField] RectTransform contentAclohol;
 
-    private List<ItemModel> items = new();
+    [SerializeField] public List<AlcoholItemModel> items = new List<AlcoholItemModel>();
 
-    private void Start()
+    void Start()
     {
-        items.Add(new ItemModel("Vodka", "vodka", 0.5f, 40, 1000));
-        items.Add(new ItemModel("Lager", "beer", 0.5f, 8, 600));
-        items.Add(new ItemModel("AppleCidre", "cidre", 0.3f, 3, 1200));
+        FillMenuList(items);
+        InstantiateAllItems(items);
+    }
 
-        foreach (ItemModel item in items)
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            var instance = GameObject.Instantiate(itemPrefab.gameObject, contentAclohol);
-            instance.transform.GetChild(0).GetChild(0).gameObject.GetComponent<TMP_Text>().text = item.label;
-            instance.transform.GetChild(1).GetChild(0).gameObject.GetComponent<TMP_Text>().text = item.type;
-            instance.transform.GetChild(2).GetChild(0).gameObject.GetComponent<TMP_Text>().text = item.volume.ToString();
-            instance.transform.GetChild(3).GetChild(0).gameObject.GetComponent<TMP_Text>().text = item.strength.ToString();
-            instance.transform.GetChild(4).GetChild(0).gameObject.GetComponent<TMP_Text>().text = item.price.ToString();
+            //SortByPrice();
         }
     }
 
+    private void InstantiateAllItems(List<AlcoholItemModel> models)
+    {
+        foreach (Transform child in contentAclohol)
+        {
+            Destroy(child.gameObject);
+        }
 
-    class ItemModel
+        foreach (var model in models)
+        {
+            var instance = GameObject.Instantiate(alcoholItemPrefab.gameObject, contentAclohol);
+            InitializeItemView(instance, model);
+        }
+    }
+
+    private void InitializeItemView(GameObject instance, AlcoholItemModel model)
+    {
+        instance.transform.GetChild(0).GetChild(0).gameObject.GetComponent<TMP_Text>().text = model.label;
+        instance.transform.GetChild(1).GetChild(0).gameObject.GetComponent<TMP_Text>().text = model.type;
+        instance.transform.GetChild(2).GetChild(0).gameObject.GetComponent<TMP_Text>().text = model.volume.ToString();
+        instance.transform.GetChild(3).GetChild(0).gameObject.GetComponent<TMP_Text>().text = model.strength.ToString();
+        instance.transform.GetChild(4).GetChild(0).gameObject.GetComponent<TMP_Text>().text = model.price.ToString();
+    }
+
+    private void FillMenuList(List<AlcoholItemModel> items)
+    {
+        items.Add(new AlcoholItemModel("Vodka", "vodka", 0.5f, 40, 1000));
+        items.Add(new AlcoholItemModel("Lager", "beer", 0.5f, 8, 600));
+        items.Add(new AlcoholItemModel("AppleCidre", "cidre", 0.3f, 3, 1200));
+    }
+
+/*    private void SortByPrice(List<AlcoholItemModel> items)
+    {
+        items.Sort();
+    }*/
+
+    public class AlcoholItemView
+    {
+        public TextMeshPro label;
+        public TextMeshPro type;
+        public TextMeshPro volume;
+        public TextMeshPro strength;
+        public TextMeshPro price;
+
+        public AlcoholItemView(TextMeshPro label, TextMeshPro type, TextMeshPro volume, TextMeshPro strength, TextMeshPro price)
+        {
+            this.label = label ?? throw new ArgumentNullException(nameof(label));
+            this.type = type ?? throw new ArgumentNullException(nameof(type));
+            this.volume = volume ?? throw new ArgumentNullException(nameof(volume));
+            this.strength = strength ?? throw new ArgumentNullException(nameof(strength));
+            this.price = price ?? throw new ArgumentNullException(nameof(price));
+        }
+    }
+
+    public class AlcoholItemModel
     {
         public string label { get; private set; }
-        public string type { get; set; }
+        public string type { get; private set; }
+        public float volume { get; private set; }
+        public float strength { get; private set; }
+        public float price { get; private set; }
 
-        public float volume, strength, price;
-        private int id, order;
-
-        public ItemModel(string label, string type, float volume, float strength, float price)
+        public AlcoholItemModel(string label, string type, float volume, float stregth, float price)
         {
-            this.label = label;
-            this.type = type;
+            this.label = label ?? throw new ArgumentNullException(nameof(label));
+            this.type = type ?? throw new ArgumentNullException(nameof(type));
             this.volume = volume;
-            this.strength = strength;
+            this.strength = stregth;
             this.price = price;
         }
     }
